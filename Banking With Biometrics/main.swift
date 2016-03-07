c082de7d2d1d19adecb9fee21c8ffe137a2173b0
Jun <cygoh@umich.edu>
2016-03-06 22:18:50 -0800

    Cleaned up some files

--- Banking With Biometrics/main.swift
+++ Banking With Biometrics/main.swift
@@ -10,10 +10,4 @@ import Foundation
 import UIKit
 import BBLibrary
 
-
-//
-//let configuration = BBConfiguration(serverIP:"localhost", domainID:"domainID", cacheSize:1024, sendRate:10, enabledComponents: [BBComponents.TouchEvents], callback: {_ in})
-//
-//let bblibrary = BBLibrary(args: configuration)
-//
-//UIApplicationMain(Process.argc, Process.unsafeArgv, NSStringFromClass(UIApp), NSStringFromClass(AppDelegate))
+UIApplicationMain(Process.argc, Process.unsafeArgv, NSStringFromClass(UIAppSub), NSStringFromClass(AppDelegate))
