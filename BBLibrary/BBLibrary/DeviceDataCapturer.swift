a9bde7c71e5bf73306701ed9f2f90a810e700794
anthony <anthony-campbell@hotmail.com>
2016-03-09 04:39:48 -0800

    added print statements

--- BBLibrary/BBLibrary/DeviceDataCapturer.swift
+++ BBLibrary/BBLibrary/DeviceDataCapturer.swift
@@ -2,11 +2,12 @@
 //  DeviceDataCapturer.swift
 //  BBLibrary
 //
-//  Created by Anthony on 2016-03-08.
+//  Created by Anthony on 2016-03-09.
 //
 //
 
 import Foundation
+
 
 class DeviceDataCapturer{
     let cache: Cache
@@ -20,7 +21,7 @@ class DeviceDataCapturer{
         let ios = UIDevice.currentDevice().systemVersion
         let iosPack = DataPacket.init(data: ["IOS: ":"\(ios)"])
         cache.store(iosPack)
-        print("IOS: \(ios)")
+        print("|| IOS: \(ios)")
     }
     
     //capture and store model (ipad or iphone)
@@ -28,7 +29,7 @@ class DeviceDataCapturer{
         let device = UIDevice.currentDevice().model
         let devicePack = DataPacket.init(data: ["Model: ":"\(device)"])
         cache.store(devicePack)
-        print("Model: \(device)")
+        print("|| Model: \(device)")
     }
     
     //capture and store device width and height
@@ -36,12 +37,12 @@ class DeviceDataCapturer{
         let screenWidth = screenSize.width
         let screenWidthPack = DataPacket.init(data: ["ScreenWidth in points: ":"\(screenWidth)"])
         cache.store(screenWidthPack)
-        print("ScreenWidth in points: \(screenWidth)")
+        print("|| ScreenWidth in points: \(screenWidth)")
         
         let screenHeight = screenSize.height
         let screenHeightPack = DataPacket.init(data: ["ScreenHeight in points: ":"\(screenHeight)"])
         cache.store(screenHeightPack)
-        print("ScreenHeight in points: \(screenHeight)")
+        print("|| ScreenHeight in points: \(screenHeight)")
     }
     
     //capture and store local time zone
@@ -49,7 +50,7 @@ class DeviceDataCapturer{
         let ltz =  NSTimeZone.localTimeZone().name
         let ltzPack = DataPacket(data: ["TimeZone: ":ltz])
         cache.store(ltzPack)
-        print("TimeZone: \(ltz)")
+        print("|| TimeZone: \(ltz)")
     }
     
     //capture and store language settings
@@ -57,7 +58,7 @@ class DeviceDataCapturer{
         let prefferedLanguage = NSLocale.preferredLanguages()[0]
         let prefferedLanguagePack = DataPacket.init(data: ["Preffered Language: ":"\(prefferedLanguage)"])
         cache.store(prefferedLanguagePack)
-        print("Preffered Language: \(prefferedLanguage)")
+        print("|| Preffered Language: \(prefferedLanguage)")
     }
     
 }
